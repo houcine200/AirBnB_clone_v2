12ede7c89f82c9b991047b68dd553f9c44609122
Elimali hamza <elimal.hamza@gmail.com>
2024-01-01 05:00:35 -0800

    pyc

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,13 +9,14 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
+
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary or a filtered list of models currently in storage"""
+        """Returns a dictionary or a filtered list of models in storage"""
         new_dict = {}
         if cls is None:
             return FileStorage.__objects
@@ -50,10 +51,9 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-
 
     def delete(self, obj=None):
         """Deletes obj from __objects if it's inside"""
