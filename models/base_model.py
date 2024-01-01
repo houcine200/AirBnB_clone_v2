511e31ff90dfee467cbeb4d38f6f78c858c37cad
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-01 13:18:15 +0100

    base model __init__

--- models/base_model.py
+++ models/base_model.py
@@ -16,18 +16,17 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            
-        else:
-            for key, value in kwargs.items():
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs:
+            for key, val in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value,
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-                if key != "__class__":
-                    self.__dict__[key] = value
+                    val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, val)
+
 
     def __str__(self):
         """Returns a string representation of the instance"""
