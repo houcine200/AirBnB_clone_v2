83cda5a4ef6251cf77f3bf3313e526f9e6e3dbba
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-11 15:28:20 +0100

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,18 +11,19 @@ sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 
 echo "<html>
-<head>
-</head>
-<body>Hello World</body>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
 if ! sudo grep -q "location /hbnb_static/" /etc/nginx/sites-enabled/default; then
-    sudo sed -i '/server_name _;/a location /hbnb_static/ {\n\talias /data/web_static/current/
-;\n}' /etc/nginx/sites-enabled/default
+    sudo sed -i '/server_name _;/a\\nlocation \/hbnb_static\/ {\n\talias \/data\/web_static\/current\/;\n}' /etc/nginx/sites-enabled/default
 fi
 
 sudo service nginx restart
