585710df82f0caed058775d67445594e6432f758
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-11 20:52:57 +0100

    2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+from datetime import datetime
+from os import path
+from fabric import local, put, run
+
+def do_pack():
+    """Create a .tgz archive from the contents of web_static folder."""
+    archive_filename = f"web_static_{datetime.now():%Y%m%d%H%M%S}.tgz"
+    archive_path = f"versions/{archive_filename}"
+
+    local("mkdir -p versions")
+    local(f"tar -cvzf {archive_path} web_static")
+    return archive_path
+
+def do_deploy(archive_path):
+    try:
+        if not path.exists(archive_path):
+            return False
+
+        put(archive_path, "/tmp/")
+        archive_filename = archive_filename = archive_path.split("/")[-1].split(".")[0]
+        release_path = f"/data/web_static/releases/{archive_filename}"
+        run(f"mkdir -p {release_path}")
+        run(f"tar -xzf /tmp/{archive_filename}.tgz -C {release_path} --strip-components=1")
+
+        run(f"rm /tmp/{archive_filename}.tgz")
+
+        current_path = "/data/web_static/current"
+        run(f"rm -f {current_path}")
+
+        run(f"ln -s {release_path} {current_path}")
+
+        return True
+
+    except Exception as e:
+        return False
