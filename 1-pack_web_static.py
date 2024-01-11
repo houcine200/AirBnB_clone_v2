371f97f0e2abef12e3288bb38be9fb453225ba74
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-11 18:08:21 +0100

    1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from datetime import datetime
 from fabric.api import local
+
+
 def do_pack():
     """Create a .tgz archive from the contents of web_static folder."""
     file_name = f"web_static_{datetime.now():%Y%m%d%H%M%S}.tgz"
