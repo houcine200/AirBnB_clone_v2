55a85e6c49eab2a657f32208734aaf1f49ded1bb
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-11 19:31:27 +0100

    1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,8 +6,8 @@ from fabric.api import local
 def do_pack():
     """Create a .tgz archive from the contents of web_static folder."""
     file_name = f"web_static_{datetime.now():%Y%m%d%H%M%S}.tgz"
+    archive_path = f"versions/{file_name}"
 
     local("mkdir -p versions")
-    """tar -czvf archive_name.tgz web_static"""
-    local(f"tar -cvzf versions/{file_name} {'web_static'}")
-    return f"versions/{file_name} {'web_static'}"
+    local(f"tar -cvzf {archive_path} web_static")
+    return archive_path
