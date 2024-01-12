97928eb1973a714e2333cec1d07e7236c67cdb70
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-12 12:09:43 +0100

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,8 +23,7 @@ def do_deploy(archive_path):
             return False
 
         put(archive_path, "/tmp/")
-        archive_filename = archive_filename =\
-            archive_path.split("/")[-1].split(".")[0]
+        archive_filename = archive_path.split("/")[-1].split(".")[0]
         release_path = f"/data/web_static/releases/{archive_filename}"
         run(f"mkdir -p {release_path}")
         run(f"tar -xzf /tmp/{archive_filename}.tgz -C {release_path}\
@@ -34,10 +33,11 @@ def do_deploy(archive_path):
 
         current_path = "/data/web_static/current"
         run(f"rm -f {current_path}")
-
         run(f"ln -s {release_path} {current_path}")
 
+        print("Deployment successful!")
         return True
 
-    except Exception as e:
+    except Exception as err:
+        print(err)
         return False
