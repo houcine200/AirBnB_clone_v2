ae82ba7a97923fbf5986b332f2a4cb3095bc1163
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-12 11:59:26 +0100

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 from datetime import datetime
 from os import path
-from fabric import local, put, run
+from fabric import local, put, run, env
+
+env.hosts = ['54.236.231.98', '	54.197.46.38']
+
 
 def do_pack():
     """Create a .tgz archive from the contents of web_static folder."""
@@ -12,16 +15,20 @@ def do_pack():
     local(f"tar -cvzf {archive_path} web_static")
     return archive_path
 
+
 def do_deploy(archive_path):
+    """Distribute an archive to web servers."""
     try:
         if not path.exists(archive_path):
             return False
 
         put(archive_path, "/tmp/")
-        archive_filename = archive_filename = archive_path.split("/")[-1].split(".")[0]
+        archive_filename = archive_filename =\
+            archive_path.split("/")[-1].split(".")[0]
         release_path = f"/data/web_static/releases/{archive_filename}"
         run(f"mkdir -p {release_path}")
-        run(f"tar -xzf /tmp/{archive_filename}.tgz -C {release_path} --strip-components=1")
+        run(f"tar -xzf /tmp/{archive_filename}.tgz -C {release_path}\
+            --strip-components=1")
 
         run(f"rm /tmp/{archive_filename}.tgz")
 
