d01003438e4bd0fcd3c6d7c06ef219cbcae80201
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-13 18:59:03 +0100

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,26 +15,19 @@ def do_deploy(archive_path):
         if not path.exists(archive_path):
             return False
 
-        put(archive_path, "/tmp/")
-
         filename = archive_path.split("/")[-1]
         folder_name = ("/data/web_static/releases/{}"
                        .format(filename.split('.')[0]))
+
+        put(archive_path, "/tmp/")
         run("mkdir -p {}".format(folder_name))
         run("tar -xzf /tmp/{} -C {}".format(filename, folder_name))
-
         run("rm /tmp/{}".format(filename))
-
         run("mv {}/web_static/* {}".format(folder_name, folder_name))
-
         run("rm -rf {}/web_static".format(folder_name))
-
         run("rm -rf /data/web_static/current")
-
         run("ln -s {} /data/web_static/current".format(folder_name))
-
         print("New version deployed!")
         return True
-
     except Exception:
         return False
