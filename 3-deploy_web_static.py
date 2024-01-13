32583a38e56cec6cf551bbf6c07cc37c432d4b6a
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-13 20:16:05 +0100

    3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Fabric script that creates and distributes an archive to your web servers"""
 
+from fabric.api import env
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 do_pack = __import__('2-do_deploy_web_static').do_pack
-from fabric.api import env
 
 
 env.hosts = ['54.236.231.98', '54.197.46.38']
@@ -15,6 +15,7 @@ def deploy():
     if not archive_path:
         return False
     return do_deploy(archive_path)
+
 
 if __name__ == "__main__":
     deploy()
