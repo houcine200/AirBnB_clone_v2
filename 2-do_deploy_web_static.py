6789036eb3ec0d990b870022477096632009e0d9
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-13 19:09:41 +0100

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
-""" a Fabric script (based on the file 1-pack_web_static.py) that distributes..
-    ..an archive to your web servers, using the function do_deploy: """
+"""Fabric script that distributes an archive to your web servers"""
 
-
-from fabric.api import *
 from datetime import datetime
-from os.path import exists
+from os import path
+from fabric.api import local, put, run, env
 
 
-env.hosts = ['54.236.231.98', '54.197.46.38']
+env.hosts = ['54.236.231.98', '	54.197.46.38']
+
 
 def do_deploy(archive_path):
-    """ distributes an archive to my web servers
-    """
-    if exists(archive_path) is False:
-        return False
-    filename = archive_path.split('/')[-1]
-    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
-    tmp = "/tmp/" + filename
-
+    """Distribute an archive to web servers."""
     try:
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}/".format(no_tgz))
-        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
-        run("rm {}".format(tmp))
-        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
-        run("rm -rf {}/web_static".format(no_tgz))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}/ /data/web_static/current".format(no_tgz))
+        if not path.exists(archive_path):
+            return False
 
+        filename = archive_path.split("/")[-1]
+        folder_name = ("/data/web_static/releases/{}"
+                       .format(filename.split('.')[0]))
+
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(folder_name))
+        run("tar -xzf /tmp/{} -C {}".format(filename, folder_name))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}/web_static/* {}".format(folder_name, folder_name))
+        run("rm -rf {}/web_static".format(folder_name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folder_name))
+        print("New version deployed!")
         return True
-    except:
+    except Exception:
         return False
