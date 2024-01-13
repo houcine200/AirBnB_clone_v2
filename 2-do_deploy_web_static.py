71cd24cba91e55fad70287991a89a57b4bfac868
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-13 19:02:48 +0100

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
-"""Fabric script that distributes an archive to your web servers"""
+""" a Fabric script (based on the file 1-pack_web_static.py) that distributes..
+    ..an archive to your web servers, using the function do_deploy: """
 
+
+from fabric.api import *
 from datetime import datetime
-from os import path
-from fabric.api import local, put, run, env
+from os.path import exists
 
 
-env.hosts = ['54.236.231.98', '	54.197.46.38']
-
+env.hosts = ['52.87.255.164', '18.204.11.196']
 
 def do_deploy(archive_path):
-    """Distribute an archive to web servers."""
+    """ distributes an archive to my web servers
+    """
+    if exists(archive_path) is False:
+        return False
+    filename = archive_path.split('/')[-1]
+    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
+    tmp = "/tmp/" + filename
+
     try:
-        if not path.exists(archive_path):
-            return False
-
-        filename = archive_path.split("/")[-1]
-        folder_name = ("/data/web_static/releases/{}"
-                       .format(filename.split('.')[0]))
-
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(folder_name))
-        run("tar -xzf /tmp/{} -C {}".format(filename, folder_name))
-        run("rm /tmp/{}".format(filename))
-        run("mv {}/web_static/* {}".format(folder_name, folder_name))
-        run("rm -rf {}/web_static".format(folder_name))
+        run("mkdir -p {}/".format(no_tgz))
+        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+        run("rm -rf {}/web_static".format(no_tgz))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_name))
-        print("New version deployed!")
+        run("ln -s {}/ /data/web_static/current".format(no_tgz))
+
         return True
-    except Exception:
+    except:
         return False
