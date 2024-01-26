46edfa79a5c946173ec0fe5bc1aef94477f7f64f
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-26 16:21:39 +0100

    0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""Starts a Flask web application."""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+    """Displays "Hello HBNB!" when the root URL is accessed."""
     return "Hello HBNB!"
 
 
