02a014651acbce2ea9f0177c8b336c7e8645d104
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-26 18:55:54 +0100

    2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays "Hello HBNB!" when the root URL is accessed."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays "HBNB" when the /hbnb URL is accessed."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=0)
+def c_route(text):
+    """Displays "C " followed by the value of the text variable."""
+    clean_text = text.replace("_", " ")
+    return f"C {clean_text}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
