b55b9b2f0b7f600e9c8a5a3d1e46d0a5c9478e0b
Houcine Walaq <houcine.walaq@gmai.com>
2024-01-26 20:20:56 +0100

    4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,1 +1,41 @@
 #!/usr/bin/python3
+"""Starts a Flask web application with specific routes."""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays "Hello HBNB!" when the root URL is accessed."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays "HBNB" when the /hbnb URL is accessed."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """Displays "C " followed by text."""
+    clean_text = text.replace("_", " ")
+    return f"C {clean_text}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is cool"):
+    """Displays "Python " followed text."""
+    clean_text = text.replace("_", " ")
+    return f"Python {clean_text}"
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def number_route(n):
+    n = int(n)
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
